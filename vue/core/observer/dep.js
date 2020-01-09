4975f2a458b09b529492823a08ad534dfa35a438
jalever <jaleverchen@outlook.com>
2020-01-09 21:00:28 +0800

    add comment

--- vue/core/observer/dep.js
+++ vue/core/observer/dep.js
@@ -1,10 +1,10 @@
 /* @flow */
 
-import type Watcher from './watcher'
-import { remove } from '../util/index'
-import config from '../config'
+import type Watcher from "./watcher";
+import { remove } from "../util/index";
+import config from "../config";
 
-let uid = 0
+let uid = 0;
 
 /**
  * A dep is an observable that can have multiple
@@ -15,36 +15,50 @@ export default class Dep {
   id: number;
   subs: Array<Watcher>;
 
-  constructor () {
-    this.id = uid++
-    this.subs = []
+  constructor() {
+    this.id = uid++;
+    this.subs = []; //被渲染所需要的数据
   }
 
-  addSub (sub: Watcher) {
-    this.subs.push(sub)
+  //依赖收集
+  addSub(sub: Watcher) {
+    this.subs.push(sub);
   }
 
-  removeSub (sub: Watcher) {
-    remove(this.subs, sub)
+  //移除渲染不需要的数据
+  removeSub(sub: Watcher) {
+    //   export function remove (arr: Array<any>, item: any): Array<any> | void {
+    //   if (arr.length) {
+    //     const index = arr.indexOf(item)
+    //     if (index > -1) {
+    //       return arr.splice(index, 1)
+    //     }
+    //   }
+    // }
+    remove(this.subs, sub);
   }
 
-  depend () {
+  depend() {
     if (Dep.target) {
-      Dep.target.addDep(this)
+      Dep.target.addDep(this);
     }
   }
 
-  notify () {
+  notify() {
     // stabilize the subscriber list first
-    const subs = this.subs.slice()
-    if (process.env.NODE_ENV !== 'production' && !config.async) {
+    const subs = this.subs.slice();
+
+    // 开发环境 && 非异步
+    if (process.env.NODE_ENV !== "production" && !config.async) {
       // subs aren't sorted in scheduler if not running async
       // we need to sort them now to make sure they fire in correct
       // order
-      subs.sort((a, b) => a.id - b.id)
+      subs.sort((a, b) => a.id - b.id);
     }
+
+    //在对data中的数据进行修改的时候setter就只会触发subs中的函数
     for (let i = 0, l = subs.length; i < l; i++) {
-      subs[i].update()
+      subs[i].update();
     }
   }
 }
@@ -52,15 +66,15 @@ export default class Dep {
 // The current target watcher being evaluated.
 // This is globally unique because only one watcher
 // can be evaluated at a time.
-Dep.target = null
-const targetStack = []
+Dep.target = null;
+const targetStack = [];
 
-export function pushTarget (target: ?Watcher) {
-  targetStack.push(target)
-  Dep.target = target
+export function pushTarget(target: ?Watcher) {
+  targetStack.push(target);
+  Dep.target = target;
 }
 
-export function popTarget () {
-  targetStack.pop()
-  Dep.target = targetStack[targetStack.length - 1]
+export function popTarget() {
+  targetStack.pop();
+  Dep.target = targetStack[targetStack.length - 1];
 }
--- vue/core/util/index.js
+++ vue/core/util/index.js
@@ -2,12 +2,12 @@
 
 
 // isDef: form shared/util
-export * from 'shared/util'
-export * from './lang'
-export * from './env'
-export * from './options'
-export * from './debug'
-export * from './props'
-export * from './error'
-export * from './next-tick'
-export { defineReactive } from '../observer/index'
+export * from 'shared/util';
+export * from './lang';
+export * from './env';
+export * from './options';
+export * from './debug';
+export * from './props';
+export * from './error';
+export * from './next-tick';
+export { defineReactive } from '../observer/index';
